6330b5be7ddbb054716191b149bec95b36d7a511
Gabriel Augusto <61918927+gabriel-adsv@users.noreply.github.com>
2023-04-30 18:26:58 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+import { Router } from './router.js'
+
+const router = new Router()
+router.add("/", "pages/home.html")
+router.add("/universe", "pages/universe.html")
+router.add("/explorer", "pages/explorer.html")
+
+router.handle()
+
+window.addEventListener(`popstate`, () => {
+  router.handle()
+})
+
+window.route = () => {
+  router.route()
+}
---
+++ js/router.js
@@ -0,0 +1,37 @@
+export class Router {
+  routes = {}
+
+  add(routeName, page) {
+    this.routes[routeName] = page
+  }
+
+  route(event) {
+    event = event || window.event
+    event.preventDefault()
+
+    window.history.pushState({}, "", event.target.href)
+
+    if(window.location.pathname == "/explorer") {
+      document.body.style.backgroundImage = "url(../assets/mountains-universe-3.png)"
+    } else if(window.location.pathname == "/universe") {
+      document.body.style.backgroundImage = "url(../assets/mountains-universe-2.png)"
+    } else {
+      document.body.style.backgroundImage = "url(../assets/mountains-universe-1.png)"
+    }
+
+    this.handle()
+  }
+
+  handle() {
+    const { pathname } = window.location
+    const route = this.routes[pathname]
+
+    fetch(route)
+    .then((data) => {
+      return data.text()
+    })
+    .then((html) => {
+      return document.querySelector(".content").innerHTML = html
+    })
+  }
+}
